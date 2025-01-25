aaca00506015423d336d1224a1bd8f3335734562
nathanxluci4 <n29436384@gmail.com>
2025-01-25 10:43:06 -0800

    Add files via upload

---
+++ plugins/dl-ytm.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+let handler = async (m, { conn, text, usedPrefix }) => {
+  if (!text) throw '✳️ What do you want me to search for on YouTube?';
+
+  try {
+    const query = encodeURIComponent(text);
+    const response = await axios.get(`https://weeb-api.vercel.app/ytsearch?query=${query}`);
+    const results = response.data;
+
+    if (results.length === 0) {
+      throw 'No results found for the given query.';
+    }
+
+    const firstResult = results[0];
+
+ const message = `
+───────────────
+✨ ${firstResult.title} ✨
+───────────────
+🖇️ *Link*: ${firstResult.url} ${firstResult.url}  
+⏱️ *Duration*: ${firstResult.timestamp}  
+📅 *Published*: ${firstResult.ago}  
+👁️ *Views*: ${firstResult.views}  
+     *MADE WITH LOVE BY XLICON V2*
+───────────────
+`;
+    await conn.sendFile(m.chat, firstResult.thumbnail, 'yts.jpeg', message, m);
+    
+    await m.react('⏳');
+    
+    const downloadUrl = `https://ironman.koyeb.app/ironman/dl/yta?url=${encodeURIComponent(firstResult.url)}`;
+    const title = firstResult.title;
+
+    await conn.sendMessage(m.chat, {
+      audio: { url: downloadUrl },
+      mimetype: 'audio/mpeg',
+      ptt: false,
+      fileName: title,
+    }, { quoted: m });
+
+  } catch (error) {
+    console.error(error);
+    throw 'An error occurred while processing your request.';
+  }
+};
+
+handler.help = ['search and dl'];
+handler.tags = ['downloader'];
+handler.command = ['ytm', 'play'];
+
+export default handler;
